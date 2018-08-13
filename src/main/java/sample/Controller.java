016f17f78781d38a275453a0f8e6f26fdfe3bb6e
deep <deepak.kumar6@ge.com>
2018-08-13 12:58:01 -0700

    reading Configurations

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -4,10 +4,14 @@ package sample;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class Controller {
@@ -19,6 +23,16 @@ public class Controller {
         names.add("Klay Thompson");
         Payload payload = new Payload(names);
         return new ResponseEntity<Payload>(payload, HttpStatus.OK);
+    }
+    @RequestMapping(value={"/configuration"},method= RequestMethod.GET)
+    public ResponseEntity<?>getConfiguration() {
+        Map javaEnvironment = System.getenv();
+        return new ResponseEntity<Map>(javaEnvironment, HttpStatus.OK);
+    }
+    @RequestMapping(value={"/configuration/{pathVariable}"},method= RequestMethod.GET)
+    public ResponseEntity<?>getConfigurationOf(@PathVariable String pathVariable) {
+        String javaEnvironment = System.getenv(pathVariable);
+        return new ResponseEntity<String>(javaEnvironment, HttpStatus.OK);
     }
 }
 
